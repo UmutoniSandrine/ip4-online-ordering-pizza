cba75f5bea091ed72ee27c6c395ebaf355e74d0e
UmutoniSandrine <umutonisandrine250@gmail.com>
2020-10-11 11:18:31 +0200

    56 commit

---
+++ javascript/script.js
@@ -0,0 +1,9 @@
+var price ,crust_price,tropping_price;
+var total=0;
+function findPizza(name,size,crust,topping,total){
+    this.name= name;
+    this.size=size;
+    this.crust=crust;
+    this.topping=topping;
+    this.total =total;
+}

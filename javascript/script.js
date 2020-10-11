393f82281999331018c2d58ef731ea4b2529e7ab
UmutoniSandrine <umutonisandrine250@gmail.com>
2020-10-11 12:43:33 +0200

    64 commit

--- javascript/script.js
+++ javascript/script.js
@@ -17,6 +17,40 @@ $(document).ready(function(){
             stopping.push($(this).val());
         });
         console.log(stopping.join(", "));
+        switch(ssize){
+            case "0":
+              price =0;
+            break;
+            case "large":
+               price = 20,000(RWF);
+               console.log(price);
+             break;
+             case "medium":
+               price = 10,000(RWF);
+               console.log("The price is "+price);
+             break;
+             case "small":
+               price = 5,000(RWF);
+               console.log(price);
+            default:
+               console.log("error"); 
+        }
+        switch(scrust){
+            case "0":
+              crust_price = 0;
+            break;
+            case "Crispy":
+              crust_price = 10,000(RWF);
+            break;
+            case "Stuffed":
+              crust_price = 5,000(RWF);
+            break;
+            case "Gluten":
+              crust_price = 2,500(RWF);
+            break;
+            default:
+              console.log("No price"); 
+          }
 
 
     });
